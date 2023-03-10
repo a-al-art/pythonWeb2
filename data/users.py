4f6bb21388f578986ef3a71dd70e5c83c99ac6e4
west1 <aaa>
2023-03-10 18:26:03 +0400

    news

--- data/users.py
+++ data/users.py
@@ -1,6 +1,8 @@
 import datetime
 import sqlalchemy
+from sqlalchemy import orm
 from .db_session import SqlAlchemyBase
+
 
 
 class User(SqlAlchemyBase):
@@ -15,3 +17,8 @@ class User(SqlAlchemyBase):
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
+
+    news = orm.relationship("News", back_populates='user')
+
+    def __repr__(self):
+        return f'{self.id} {self.name} {self.email} {self.created_date}'
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from flask import Flask, url_for, request, render_template, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
-
+from werkzeug.security import generate_password_hash, check_password_hash
+from data.news import News
 from data import db_session
 from data.users import User
 
@@ -10,6 +11,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 user = "Ученик Лицея Академии Яндекса"
+
+DEBUG_MODE = True
 
 
 class LoginForm(FlaskForm):
@@ -43,19 +46,38 @@ def second_page():
 
 def main():
     db_session.global_init("db/blogs.db")
-    make_user()
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).first()
+    if user is None:
+        tmp_fill_base()
+        user = db_sess.query(User).first()
+    if DEBUG_MODE:
+        print(user.name)
+        for user in db_sess.query(User).all():
+            print(user)
+
+
+
+
     app.run()  # app.run(port=8080, host='127.0.0.1')
 
-def make_user():
+
+def tmp_fill_base():
     user = User()
-    user.name = "Пользователь 1"
+    user.name = "Админ"
     user.about = "биография пользователя 1"
-    user.email = "email@email.ru"
+    user.email = "admin@email.com"
     db_sess = db_session.create_session()
     db_sess.add(user)
     db_sess.commit()
-
+    news = News(title="Первая новость", content="Привет блог!",
+                user_id=1, is_private=False)
+    db_sess.add(news)
+    db_sess.commit()
 
 
 if __name__ == '__main__':
+    x = generate_password_hash('123')
+    print(x)
+    print(check_password_hash(x, '123'))
     main()
